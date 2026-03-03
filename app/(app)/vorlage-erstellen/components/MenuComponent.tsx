70f215708d895d7988cbc90d339929b5cbb27fbb
henrymh5 <info@kortewebdesign.de>
2026-03-03 10:25:38 +0100

    feat: Dragging elements enabled and zooming in out of canvas

--- app/(app)/vorlage-erstellen/components/MenuComponent.tsx
+++ app/(app)/vorlage-erstellen/components/MenuComponent.tsx
@@ -1,16 +1,65 @@
+import { useEffect, useState } from "react";
+
 interface MenuComponentProps{
     title: string;
     image_url: string;
     category: string;
+    handleDrag: () => void;
+    changeSelectedItem: (e: React.PointerEvent) => void; 
+    dragging: boolean
 }
 
-export default function MenuComponent({title, image_url, category}: MenuComponentProps) {
+export default function MenuComponent({title, image_url, category, handleDrag, changeSelectedItem, dragging}: MenuComponentProps) {
     const componentCategory = category;
     
+    useEffect(() => {
+        if (dragging) window.addEventListener("pointerup", handleDrag)
+        return () => {
+            window.removeEventListener("pointerup", handleDrag)
+        }
+    }, [dragging])
+
     return (
-            <div className="flex cursor-grab click:cursor-grabbing flex-col gap-2 overflow-hidden basis-[48%] text-center flex-none items-center justify-center aspect-1/1 text-[#ffffffa5] text-sm">
+            <div onPointerDown={(e) => {changeSelectedItem(e); handleDrag()}} className="flex cursor-grab click:cursor-grabbing flex-col gap-2 overflow-hidden basis-[48%] text-center flex-none items-center justify-center aspect-1/1 text-[#ffffffa5] text-sm">
                 <div style={{backgroundImage: `url('/${image_url}')`}} className="bg-[#05261e] self-stretch flex-1 bg-no-repeat bg-cover rounded-xl"></div>
                 {title}
             </div>
+    )
+}
+
+interface DraggableItemProps{
+    selected: { type: string; image_url: string; }; 
+    isDragging: boolean;
+}
+
+export function SelectedDraggable({selected, isDragging}: DraggableItemProps){
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+
+        useEffect(() => {
+            const updatePosition = (e:PointerEvent) => {
+                setPosition({x: e.clientX, y: e.clientY})
+            }
+
+            if(isDragging){
+                window.addEventListener("pointerdown", updatePosition)
+                window.addEventListener("pointermove", updatePosition)
+            } 
+            
+            return () => {
+                window.removeEventListener("pointermove", updatePosition)
+            }
+        }, [isDragging])
+
+    return (
+        <div
+        style={{
+            display: `${isDragging ? "flex" : "none"}`,
+            top: `${position.y}px`,
+            left: `${position.x}px`
+        }}
+        className="fixed pointer-events-none w-[100px] cursor-grabbing flex-col gap-2 overflow-hidden text-center items-center justify-center aspect-1/1 text-[#ffffffa5] text-sm">
+            <div style={{backgroundImage: `url('/${selected.image_url}')`}} className="bg-[#05261e] self-stretch flex-1 bg-no-repeat bg-cover rounded-xl"></div>
+        </div> 
     )
 }
--- app/(app)/vorlage-erstellen/page.tsx
+++ app/(app)/vorlage-erstellen/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import { Menu, Minus, Plus } from "lucide-react";
-import MenuComponent from "./components/MenuComponent";
-import SelectedDraggable from "./components/SelectedDraggable";
-import { useState } from "react";
+import MenuComponent, {SelectedDraggable} from "./components/MenuComponent";
+import { useState, useEffect } from "react";
 
 export default function CreateTemplate() {
   return (
@@ -15,26 +14,63 @@ export default function CreateTemplate() {
 }
 
 function TemplateCanvas() {
+  const [size, setSize] = useState(95)
   return (
-    <div className="flex-1 relative self-stretch">
-      <div className="TemplateCanvas absolute top-10 left-[50%] bg-white shadow-md aspect-100/141 h-[95%]">
+    <div className="flex-1 relative self-stretch overflow-scroll">
+      <div className="TemplateCanvas absolute top-10 left-[50%] bg-white shadow-md aspect-100/141" style={{height: `${size}%`}}>
 
       </div>
       <div className="Controls absolute bottom-0 right-10 text-2xl flex flex-col gap-2">
-        <div className="Plus w-10 h-10 bg-[#0A372C] text-white flex items-center rounded-full justify-center cursor-pointer"><Plus size={15} /></div>
-        <div className="Minus w-10 h-10 bg-[#0A372C] text-white rounded-full flex items-center justify-center opacity-25 cursor-pointer"><Minus size={15}/></div>
+        <div onClick={() => setSize(size <= 150 ? size + 10 : size)} className="Plus w-10 h-10 bg-[#0A372C] text-white flex items-center rounded-full justify-center cursor-pointer"><Plus size={15} /></div>
+        <div onClick={() => setSize(size >= 50 ? size - 10 : size)} className="Minus w-10 h-10 bg-[#0A372C] text-white rounded-full flex items-center justify-center opacity-25 cursor-pointer"><Minus size={15}/></div>
       </div>
     </div>
   );
 }
 
 function ComponentMenu() {
-  const [itemSelected, setItemSelected] = useState("none");
+  const [dragging, setDragging] = useState(false);
+  const [itemSelected, setItemSelected] = useState({ type: "none", image_url: "none" });
+
+  const menuItems = [
+    {
+      "type": "customer",
+      "image_url": "customer-details-bg.png"
+    },
+    {
+      "type": "invoice",
+      "image_url": "invoice-details-bg.png"
+    },
+    {
+      "type": "business",
+      "image_url": "business-information-bg.png"
+    },
+    {
+      "type": "invoice-items",
+      "image_url": "invoice-items-bg.png"
+    },
+    {
+      "type": "row",
+      "image_url": "rows-bg.png"
+    },
+    {
+      "type": "cols",
+      "image_url": "cols-bg.png"
+    },
+    {
+      "type": "new-page",
+      "image_url": "add-page-bg.png"
+    },
+    {
+      "type": "tables",
+      "image_url": "tables-bg.png"
+    }
+  ]
 
   return (
     <>
-    <SelectedDraggable image_url="customer-details-bg.png" selected={itemSelected} />
-    <div className="flex flex-col gap-5 self-stretch bg-[#0A372C] w-full max-w-[350px] rounded-2xl py-5 px-3 overflow-scroll">
+    <SelectedDraggable selected={itemSelected} isDragging={dragging} />
+    <div className="flex select-none flex-col gap-5 self-stretch bg-[#0A372C] w-full max-w-[350px] rounded-2xl py-5 px-3 overflow-scroll">
       <h3 className="text-xl text-white font-semibold">Vorlagen Bausteine</h3>
       <input
         type="text"
@@ -45,21 +81,21 @@ function ComponentMenu() {
       <div className="flex flex-wrap justify-start items-start gap-2">
         <div className="w-[100%] mb-2">
           <h4 className="text-md text-white">Rechtlich notwendig</h4>
-        </div>
-        <MenuComponent title="Kundendaten" image_url="customer-details-bg.png" category="mandatory" />
-        <MenuComponent title="Belegverweise" image_url="invoice-details-bg.png" category="mandatory" />
-        <MenuComponent title="Unternehmensangaben" image_url="business-information-bg.png" category="mandatory" />
-        <MenuComponent title="Rechnungsposten" image_url="invoice-items-bg.png" category="mandatory" />
+        </div> 
+        <MenuComponent changeSelectedItem={() => setItemSelected(menuItems[0])} handleDrag={() => setDragging(!dragging)} dragging={dragging} title="Kundendaten" image_url="customer-details-bg.png" category="mandatory" />
+        <MenuComponent changeSelectedItem={() => setItemSelected(menuItems[1])} handleDrag={() => setDragging(!dragging)} dragging={dragging} title="Belegverweise" image_url="invoice-details-bg.png" category="mandatory" />
+        <MenuComponent changeSelectedItem={() => setItemSelected(menuItems[2])} handleDrag={() => setDragging(!dragging)} dragging={dragging} title="Unternehmensangaben" image_url="business-information-bg.png" category="mandatory" />
+        <MenuComponent changeSelectedItem={() => setItemSelected(menuItems[3])} handleDrag={() => setDragging(!dragging)} dragging={dragging} title="Rechnungsposten" image_url="invoice-items-bg.png" category="mandatory" />
       </div>
 
       <div className="flex flex-wrap justify-start items-start gap-2">
         <div className="w-[100%] mb-2">
           <h4 className="text-md text-white">Layout</h4>
         </div>
-        <MenuComponent title="Reihen" image_url="rows-bg.png" category="mandatory" />
-        <MenuComponent title="Spalten" image_url="cols-bg.png" category="mandatory" />
-        <MenuComponent title="Neue Seite" image_url="add-page-bg.png" category="mandatory" />
-        <MenuComponent title="Tabelle" image_url="tables-bg.png" category="mandatory" />
+        <MenuComponent changeSelectedItem={() => setItemSelected(menuItems[4])} handleDrag={() => setDragging(!dragging)} dragging={dragging} title="Reihen" image_url="rows-bg.png" category="mandatory" />
+        <MenuComponent changeSelectedItem={() => setItemSelected(menuItems[5])} handleDrag={() => setDragging(!dragging)} dragging={dragging} title="Spalten" image_url="cols-bg.png" category="mandatory" />
+        <MenuComponent changeSelectedItem={() => setItemSelected(menuItems[6])} handleDrag={() => setDragging(!dragging)} dragging={dragging} title="Neue Seite" image_url="add-page-bg.png" category="mandatory" />
+        <MenuComponent changeSelectedItem={() => setItemSelected(menuItems[7])} handleDrag={() => setDragging(!dragging)} dragging={dragging} title="Tabelle" image_url="tables-bg.png" category="mandatory" />
       </div>
     </div>
     </>
